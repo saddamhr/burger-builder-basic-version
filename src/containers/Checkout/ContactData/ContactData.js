ebaf9dd8b242f7e3b479d08f3a621eef765568d0
saddamhr <saddambubt65@gmail.com>
2020-08-04 17:19:25 +0600

    accessing protected resources

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -109,8 +109,7 @@ class ContactData extends Component {
 
         }
 
-        this.props.onOrderBurger(order);
-        // console.log(order['price'])
+        this.props.onOrderBurger(order, this.props.token);
 
     }
 
@@ -130,6 +129,16 @@ class ContactData extends Component {
 
         if (rules.maxLength) {
             isValid = value.length >= rules.minLength && isValid
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
         }
 
         return isValid;
@@ -194,13 +203,14 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        loading: state.order.loading
+        loading: state.order.loading,
+        token: state.auth.token
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+        onOrderBurger: (orderData, token) => dispatch(actions.purchaseBurger(orderData, token))
     }
 
 }
